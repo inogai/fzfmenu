f941a1ae855f5400364ae5c384bb558f9482ed6a
inogai <inogai.sign@gmail.com>
2025-11-06 12:40:18 +0800

    chore: build with nix

---
+++ flake.nix
@@ -0,0 +1,61 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cargo2nix.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "cargo2nix.cachix.org-1:ge7JNQYaRs+DO1o50vOUxRqLVze6G2VpPTt8EAg/b50="
+    ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {self, ...} @ inputs: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+    forEachSupportedSystem = f:
+      inputs.nixpkgs.lib.genAttrs supportedSystems (
+        system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+            };
+          }
+      );
+  in {
+    devShells = forEachSupportedSystem (
+      {pkgs}: {
+        default = pkgs.mkShellNoCC {
+          packages = with pkgs; [
+            rustc
+            cargo
+            clippy
+            rustfmt
+            cargo-deny
+            cargo-edit
+            cargo-watch
+            rust-analyzer
+          ];
+
+          env = {
+            RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+          };
+        };
+      }
+    );
+
+    packages = forEachSupportedSystem (
+      {pkgs}: {
+        default = (pkgs.callPackage ./Cargo.nix {}).rootCrate.build;
+      }
+    );
+  };
+}
